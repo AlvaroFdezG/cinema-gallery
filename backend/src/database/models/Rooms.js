df88340d0ce17999f7a14c83ca805b946a696233
Alvaro <alvarofdezg@proton.me>
2026-09-17 21:24:18 +0200

    Campos añadidos a la tabla salas

--- backend/src/database/models/Rooms.js
+++ backend/src/database/models/Rooms.js
@@ -9,6 +9,14 @@ Room.init(
             type: DataTypes.INTEGER,
             primaryKey: true,
             autoIncrement: true
+        },
+        date: {
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        movie_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false
         }
     }
 )
