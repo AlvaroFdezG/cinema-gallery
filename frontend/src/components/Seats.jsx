a092c9d2d146d7abcbbaeab1206505fc62c4c214
Alvaro <alvarofdezg@proton.me>
2026-09-25 00:07:54 +0200

    Añadida simulación de cálculo de precios

--- frontend/src/components/Seats.jsx
+++ frontend/src/components/Seats.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import { getSeats } from '../services/movieService'
 import freeSeat from "./../assets/seat.png"
+import { Link } from 'react-router-dom';
 
 const Seats = () => {
 
   const [seatsArray, setSeatsArray] = useState();
   const [seatsSelected, setSeatsSelected] = useState([]);
+  const [charge, setCharge] = useState(0);
 
 
   useEffect(() => {
@@ -21,20 +23,31 @@ const Seats = () => {
 
   const addSeats = (idSeat) => {
     const seatsAdd = [...seatsSelected];
-    seatsAdd.push(idSeat);
-    setSeatsSelected(seatsAdd);
+
+    if (seatsAdd.includes(idSeat)) {
+      setSeatsSelected(seatsAdd.filter(seat => seat !== idSeat));
+      setCharge(charge - 6.50);
+    } else {
+      seatsAdd.push(idSeat);
+      setSeatsSelected(seatsAdd);
+      setCharge(charge + 6.50);
+    }
   }
 
   return (
     <div>
-      <div className='bg-[#89bcff] w-full h-10 text-white text-center mb-8 pt-2 rounded-lg'>Pantalla</div>
+      <div className='bg-blue-400 w-full h-10 text-white text-center mb-8 pt-2 rounded-lg'>Pantalla</div>
       <section className='grid grid-cols-[repeat(20,minmax(0,1fr))]'>
         {seatsArray.map((seat) => (
-          <button onClick={() => addSeats(seat.id)} className={`p-1 ${seatsSelected.includes(seat.id)? "bg-blue-600" : ""} text-center`} key={seat.id}>
+          <button onClick={() => addSeats(seat.id)} className={`p-1 ${seatsSelected.includes(seat.id) ? "bg-blue-600" : ""} text-center`} key={seat.id}>
             <img src={freeSeat} alt="seat" />
           </button>
         ))}
       </section>
+      <div className='mt-10 flex items-center gap-8 justify-center'>
+        <p className='w-full text-end text-3xl'>Precio: <span className='font-bold'>{charge} €</span></p>
+        <Link className='bg-blue-400 rounded-lg p-3 flex items-center justify-center'>Comprar <i className="fa-solid fa-angles-right"></i></Link>
+      </div>
     </div>
   )
 
